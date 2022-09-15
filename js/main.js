9902f071861db3eff743fceade0f2741e01f091a
VKhodyrev <vkhodyrev1@gmail.com>
2022-09-15 18:12:46 +0300

    implement functionality to diaplay and move obstacle

--- js/main.js
+++ js/main.js
@@ -2,10 +2,25 @@
 class Game {
     constructor(){
         this.player = null; //will store an instance of the class Player
+        this.obstacles = []; //will store instances of the class Obstacle
     }
+
     start(){
         this.player = new Player();
         this.attachEventListeners();
+        
+        setInterval(() => {
+            this.obstacles.forEach(element => {
+                element.moveDown();
+            });
+        }, 200);
+
+        setInterval(() => {
+            //create new obstacle
+            const newObstacle = new Obstacle();
+            newObstacle.positionX = Math.random()*100;
+            this.obstacles.push(newObstacle);
+        }, 2000);
     }
     attachEventListeners(){
         document.addEventListener("keydown", (event) => {
@@ -55,6 +70,41 @@ class Player {
 }
 
 
+class Obstacle {
+    constructor(){
+        this.positionX = 50;
+        this.positionY = 100;
+        this.width = 10;
+        this.height = 10;
+        this.domElement = null;
+
+        this.createDomElement();
+    }
+    createDomElement(){
+        // create dom element
+        this.domElement = document.createElement('div');
+
+        // set id and css
+        this.domElement.className = "obstacle";
+        this.domElement.style.width = this.width + "vw";
+        this.domElement.style.height = this.height + "vh";
+        this.domElement.style.bottom = this.positionY + "vh";
+        this.domElement.style.left = this.positionX + "vw";
+
+        // append to the dom
+        const boardElm = document.getElementById("board");
+        boardElm.appendChild(this.domElement)
+    }
+    moveDown(){
+        if (this.positionY > 0) {
+            this.positionY--;
+        } else {
+            this.positionY = 100;
+        }
+        this.domElement.style.bottom = this.positionY + "vh";
+        this.domElement.style.left = this.positionX + "vw";
+    }
+}
 
 const game = new Game();
 game.start();
